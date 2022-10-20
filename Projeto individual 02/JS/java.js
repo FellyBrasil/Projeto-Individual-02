d960ef9ad01a4a5d82761acad2cb9965c8fd6e2b
FellyBrasil <113535234+FellyBrasil@users.noreply.github.com>
2022-10-20 11:58:52 -0300

    Add files via upload

--- Projeto individual 02/JS/java.js
+++ Projeto individual 02/JS/java.js
@@ -1,74 +1,249 @@
-const selecione = document.querySelector(".select");
-const incremento = document.querySelector(".chave-escolha");
-const btn = document.querySelector("button");
-const radiobtn = document.querySelector(".radio-button");
-const codificar = document.querySelector("#codificar");
-const decodificar = document.querySelector("#decodificar");
+function myFunction() {
 
-
-
-
-selecione.addEventListener("click", function () {
-    if (selecione.value == "cifra") {
-      incremento.style.display = "block";
-    } else {
-      incremento.style.display = "none";
+    var texto = prompt("Qual o texto para codificar ou decodificar?")
+    var modo = prompt("Digite 1 para codificar e 2 para decodificar")
+    
+        if (modo === "1") {
+        // Encryptar BASE64 //
+        const aMyUTF8Input = strToUTF8Arr(texto);
+        const sMyBase64 = base64EncArr(aMyUTF8Input);
+        var retorno = sMyBase64;
+    
+        alert("CODIFICADO: " + retorno)
+    
+      }
+      else {
+        // Decryptar BASE64 //
+        const aMyUTF8Output = base64DecToArr(texto);
+        const sMyOutput = UTF8ArrToStr(aMyUTF8Output);
+        var retorno = sMyOutput;
+    
+        alert("DECODIFICADO: " + retorno)
+    
+      }
+    
     }
-  });
-//evento simples para a modificação da chave de escolha.
-  function base64() {
-    let mensagem = document.querySelector("#mensagem").value;
- 
-    if (codificar.checked) {
-      let codificado = btoa(mensagem);
-      return codificado;
-    } else if (decodificar.checked) {
-      let decodificado = atob(mensagem);
-      return decodificado;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Script milagroso do BASE64 //
+    "use strict";
+    // Array of bytes to Base64 string decoding
+    function b64ToUint6(nChr) {
+      return nChr > 64 && nChr < 91
+        ? nChr - 65
+        : nChr > 96 && nChr < 123
+          ? nChr - 71
+          : nChr > 47 && nChr < 58
+            ? nChr + 4
+            : nChr === 43
+              ? 62
+              : nChr === 47
+                ? 63
+                : 0;
     }
-  }//configuração inicial da base64 quando adicionado a mensagem.
-
-  function cifraCesar() {
-    let msg = document.querySelector("#mensagem").value;
-    let chave = parseInt(document.querySelector("#rangenumber").value);
-    let saida = '';
- 
-    if (codificar.checked) {
-      for (let i = 0; i < msg.length; i++) {
-        if (msg[i] === msg[i].toUpperCase()) {
-          saida += String.fromCharCode((msg.charCodeAt(i) + chave - 65) % 26 + 65);
-        } else {
-          saida += String.fromCharCode((msg.charCodeAt(i) + chave - 97) % 26 + 97);
+    
+    function base64DecToArr(sBase64, nBlocksSize) {
+      const sB64Enc = sBase64.replace(/[^A-Za-z0-9+/]/g, "");
+      const nInLen = sB64Enc.length;
+      const nOutLen = nBlocksSize
+        ? Math.ceil(((nInLen * 3 + 1) >> 2) / nBlocksSize) * nBlocksSize
+        : (nInLen * 3 + 1) >> 2;
+      const taBytes = new Uint8Array(nOutLen);
+    
+      let nMod3;
+      let nMod4;
+      let nUint24 = 0;
+      let nOutIdx = 0;
+      for (let nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+        nMod4 = nInIdx & 3;
+        nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << (6 * (3 - nMod4));
+        if (nMod4 === 3 || nInLen - nInIdx === 1) {
+          nMod3 = 0;
+          while (nMod3 < 3 && nOutIdx < nOutLen) {
+            taBytes[nOutIdx] = (nUint24 >>> ((16 >>> nMod3) & 24)) & 255;
+            nMod3++;
+            nOutIdx++;
+          }
+          nUint24 = 0;
         }
       }
-      return saida;
- 
-    } else if (decodificar.checked) {
-      for (let i = 0; i < msg.length; i++) {
-        if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
-          saida += String.fromCharCode((msg.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
-        } else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
-          saida += String.fromCharCode((msg.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
-        } else {
-          saida += String.fromCharCode(msg.charCodeAt(i));
+    
+      return taBytes;
+    }
+    
+    /* Base64 string to array encoding */
+    function uint6ToB64(nUint6) {
+      return nUint6 < 26
+        ? nUint6 + 65
+        : nUint6 < 52
+          ? nUint6 + 71
+          : nUint6 < 62
+            ? nUint6 - 4
+            : nUint6 === 62
+              ? 43
+              : nUint6 === 63
+                ? 47
+                : 65;
+    }
+    
+    function base64EncArr(aBytes) {
+      let nMod3 = 2;
+      let sB64Enc = "";
+    
+      const nLen = aBytes.length;
+      let nUint24 = 0;
+      for (let nIdx = 0; nIdx < nLen; nIdx++) {
+        nMod3 = nIdx % 3;
+        if (nIdx > 0 && ((nIdx * 4) / 3) % 76 === 0) {
+          sB64Enc += "\r\n";
+        }
+    
+        nUint24 |= aBytes[nIdx] << ((16 >>> nMod3) & 24);
+        if (nMod3 === 2 || aBytes.length - nIdx === 1) {
+          sB64Enc += String.fromCodePoint(
+            uint6ToB64((nUint24 >>> 18) & 63),
+            uint6ToB64((nUint24 >>> 12) & 63),
+            uint6ToB64((nUint24 >>> 6) & 63),
+            uint6ToB64(nUint24 & 63)
+          );
+          nUint24 = 0;
         }
       }
-      return saida;
+      return (
+        sB64Enc.substr(0, sB64Enc.length - 2 + nMod3) +
+        (nMod3 === 2 ? "" : nMod3 === 1 ? "=" : "==")
+      );
     }
-  }
-  radiobtn.addEventListener("click", function () {
-    if (codificar.checked) {
-      btn.innerHTML = "Codificar Mensagem!";
-    } else if (decodificar.checked) {
-      btn.innerHTML = "Decodificar Mensagem!";
+    
+    /* UTF-8 array to JS string and vice versa */
+    
+    function UTF8ArrToStr(aBytes) {
+      let sView = "";
+      let nPart;
+      const nLen = aBytes.length;
+      for (let nIdx = 0; nIdx < nLen; nIdx++) {
+        nPart = aBytes[nIdx];
+        sView += String.fromCodePoint(
+          nPart > 251 && nPart < 254 && nIdx + 5 < nLen /* six bytes */
+            ? /* (nPart - 252 << 30) may be not so safe in ECMAScript! So…: */
+            (nPart - 252) * 1073741824 +
+            ((aBytes[++nIdx] - 128) << 24) +
+            ((aBytes[++nIdx] - 128) << 18) +
+            ((aBytes[++nIdx] - 128) << 12) +
+            ((aBytes[++nIdx] - 128) << 6) +
+            aBytes[++nIdx] -
+            128
+            : nPart > 247 && nPart < 252 && nIdx + 4 < nLen /* five bytes */
+              ? ((nPart - 248) << 24) +
+              ((aBytes[++nIdx] - 128) << 18) +
+              ((aBytes[++nIdx] - 128) << 12) +
+              ((aBytes[++nIdx] - 128) << 6) +
+              aBytes[++nIdx] -
+              128
+              : nPart > 239 && nPart < 248 && nIdx + 3 < nLen /* four bytes */
+                ? ((nPart - 240) << 18) +
+                ((aBytes[++nIdx] - 128) << 12) +
+                ((aBytes[++nIdx] - 128) << 6) +
+                aBytes[++nIdx] -
+                128
+                : nPart > 223 && nPart < 240 && nIdx + 2 < nLen /* three bytes */
+                  ? ((nPart - 224) << 12) +
+                  ((aBytes[++nIdx] - 128) << 6) +
+                  aBytes[++nIdx] -
+                  128
+                  : nPart > 191 && nPart < 224 && nIdx + 1 < nLen /* two bytes */
+                    ? ((nPart - 192) << 6) + aBytes[++nIdx] - 128
+                    : /* nPart < 127 ? */ /* one byte */
+                    nPart
+        );
+      }
+      return sView;
     }
-  });
- 
-  btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (selecione.value === "base64") {
-      resultado.innerText = base64();
-    } else if (selecione.value === "cifra") {
-      resultado.innerText = cifraCesar();
+    
+    function strToUTF8Arr(sDOMStr) {
+      let aBytes;
+      let nChr;
+      const nStrLen = sDOMStr.length;
+      let nArrLen = 0;
+    
+      /* mapping… */
+      for (let nMapIdx = 0; nMapIdx < nStrLen; nMapIdx++) {
+        nChr = sDOMStr.codePointAt(nMapIdx);
+    
+        if (nChr > 65536) {
+          nMapIdx++;
+        }
+    
+        nArrLen +=
+          nChr < 0x80
+            ? 1
+            : nChr < 0x800
+              ? 2
+              : nChr < 0x10000
+                ? 3
+                : nChr < 0x200000
+                  ? 4
+                  : nChr < 0x4000000
+                    ? 5
+                    : 6;
+      }
+    
+      aBytes = new Uint8Array(nArrLen);
+    
+      /* transcription… */
+      let nIdx = 0;
+      let nChrIdx = 0;
+      while (nIdx < nArrLen) {
+        nChr = sDOMStr.codePointAt(nChrIdx);
+        if (nChr < 128) {
+          /* one byte */
+          aBytes[nIdx++] = nChr;
+        } else if (nChr < 0x800) {
+          /* two bytes */
+          aBytes[nIdx++] = 192 + (nChr >>> 6);
+          aBytes[nIdx++] = 128 + (nChr & 63);
+        } else if (nChr < 0x10000) {
+          /* three bytes */
+          aBytes[nIdx++] = 224 + (nChr >>> 12);
+          aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
+          aBytes[nIdx++] = 128 + (nChr & 63);
+        } else if (nChr < 0x200000) {
+          /* four bytes */
+          aBytes[nIdx++] = 240 + (nChr >>> 18);
+          aBytes[nIdx++] = 128 + ((nChr >>> 12) & 63);
+          aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
+          aBytes[nIdx++] = 128 + (nChr & 63);
+          nChrIdx++;
+        } else if (nChr < 0x4000000) {
+          /* five bytes */
+          aBytes[nIdx++] = 248 + (nChr >>> 24);
+          aBytes[nIdx++] = 128 + ((nChr >>> 18) & 63);
+          aBytes[nIdx++] = 128 + ((nChr >>> 12) & 63);
+          aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
+          aBytes[nIdx++] = 128 + (nChr & 63);
+          nChrIdx++;
+        } /* if (nChr <= 0x7fffffff) */ else {
+          /* six bytes */
+          aBytes[nIdx++] = 252 + (nChr >>> 30);
+          aBytes[nIdx++] = 128 + ((nChr >>> 24) & 63);
+          aBytes[nIdx++] = 128 + ((nChr >>> 18) & 63);
+          aBytes[nIdx++] = 128 + ((nChr >>> 12) & 63);
+          aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
+          aBytes[nIdx++] = 128 + (nChr & 63);
+          nChrIdx++;
+        }
+        nChrIdx++;
+      }
+    
+      return aBytes;
     }
-  });
